ddc26cfcf665111c270c727e933c073900a85f62
Apple <morcodou@hotmail.com>
2020-11-24 22:21:37 -0500

    add no watch config when testing karma

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
